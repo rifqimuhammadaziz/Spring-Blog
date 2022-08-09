eeae74a047f8dd489282a70fca9998a67e65af29
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-08-09 11:32:52 +0700

    setup blog resource & add post detail service

--- Blog/src/main/java/rifqimuhammadaziz/Blog/BlogApplication.java
+++ Blog/src/main/java/rifqimuhammadaziz/Blog/BlogApplication.java
@@ -1,10 +1,21 @@
 package rifqimuhammadaziz.Blog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"rifqimuhammadaziz.Library.*", "rifqimuhammadaziz.Blog.*"})
+@EnableJpaRepositories(value = "rifqimuhammadaziz.Library.repository")
+@EntityScan(value = "rifqimuhammadaziz.Library.model")
 public class BlogApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
--- Blog/src/main/java/rifqimuhammadaziz/Blog/controller/HomeController.java
+++ Blog/src/main/java/rifqimuhammadaziz/Blog/controller/HomeController.java
@@ -1,13 +1,37 @@
 package rifqimuhammadaziz.Blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import rifqimuhammadaziz.Library.dto.PostDto;
+import rifqimuhammadaziz.Library.model.Post;
+import rifqimuhammadaziz.Library.service.contract.PostService;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home() {
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+    public String home(Model model) {
+        List<PostDto> posts = postService.findAll();
+        model.addAttribute("posts", posts);
+        System.out.println(posts);
         return "index";
+    }
+
+    @GetMapping("/post/{id}")
+    public String post(@PathVariable("id") Long id, Model model) {
+        PostDto postDto = postService.findById(id);
+        model.addAttribute("post", postDto);
+
+        return "posts/post-detail";
     }
 }
