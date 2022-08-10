d52c00dc2e2d437182a59bdbdd7d62354a323ecb
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-08-10 19:19:04 +0700

    fix some view

--- Blog/src/main/java/rifqimuhammadaziz/Blog/controller/PostController.java
+++ Blog/src/main/java/rifqimuhammadaziz/Blog/controller/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import rifqimuhammadaziz.Library.dto.PostDto;
 import rifqimuhammadaziz.Library.model.Post;
+import rifqimuhammadaziz.Library.model.PostCategory;
+import rifqimuhammadaziz.Library.service.contract.PostCategoryService;
 import rifqimuhammadaziz.Library.service.contract.PostService;
 
 import java.util.List;
@@ -17,6 +19,9 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private PostCategoryService postCategoryService;
+
     @GetMapping("/post/{id}")
     public String post(@PathVariable("id") Long id, Model model) {
         PostDto postDto = postService.findById(id);
@@ -25,6 +30,9 @@ public class PostController {
         Long categoryId = postDto.getPostCategory().getId();
         List<Post> posts = postService.getRelatedPost(categoryId);
         model.addAttribute("posts", posts);
+
+        List<PostCategory> postCategories = postCategoryService.findAll();
+        model.addAttribute("postCategories", postCategories);
 
         return "posts/post-detail";
     }
