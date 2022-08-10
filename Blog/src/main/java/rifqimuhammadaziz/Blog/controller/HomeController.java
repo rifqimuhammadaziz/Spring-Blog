667a0ed82acb050737ff3eb764641a124fdc0291
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-08-10 09:55:37 +0700

    add category list on navbar

--- Blog/src/main/java/rifqimuhammadaziz/Blog/controller/HomeController.java
+++ Blog/src/main/java/rifqimuhammadaziz/Blog/controller/HomeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import rifqimuhammadaziz.Library.dto.PostDto;
 import rifqimuhammadaziz.Library.model.Post;
+import rifqimuhammadaziz.Library.model.PostCategory;
+import rifqimuhammadaziz.Library.service.contract.PostCategoryService;
 import rifqimuhammadaziz.Library.service.contract.PostService;
 
 import java.util.List;
@@ -19,11 +21,16 @@ public class HomeController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private PostCategoryService postCategoryService;
+
     @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String home(Model model) {
         List<PostDto> posts = postService.findAll();
         model.addAttribute("posts", posts);
-        System.out.println(posts);
+
+        List<PostCategory> postCategories = postCategoryService.findAll();
+        model.addAttribute("postCategories", postCategories);
         return "index";
     }
 
